8bfab9aff456e96929bbf075fb9957f6bc1fb3e3
Oleksii Karpov <alneon1@gmail.com>
2025-01-28 09:05:31 +0200

    minor improvements added v.2

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,18 @@ import { getTodos, USER_ID } from './api/todos';
 import { Todo } from './types/Todo';
 import { TodoList } from './components/TodoList';
 
+const createNewTodo = (todos: Todo[], title: string, userId: number): Todo => {
+  const newId =
+    todos.length > 0 ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
+
+  return {
+    id: newId,
+    userId,
+    title,
+    completed: false,
+  };
+};
+
 export const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [query, setQuery] = useState('');
@@ -19,15 +31,14 @@ export const App: React.FC = () => {
 
   const filteredTodos = useMemo(() => {
     return todos.filter(todo => {
-      if (filter === 'Active') {
-        return !todo.completed;
+      switch (filter) {
+        case 'Active':
+          return !todo.completed;
+        case 'Completed':
+          return todo.completed;
+        default:
+          return true;
       }
-
-      if (filter === 'Completed') {
-        return todo.completed;
-      }
-
-      return true;
     });
   }, [todos, filter]);
 
@@ -89,14 +100,7 @@ export const App: React.FC = () => {
       return;
     }
 
-    const newId =
-      todos.length > 0 ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
-    const newTodo: Todo = {
-      id: newId,
-      userId: USER_ID,
-      title: query,
-      completed: false,
-    };
+    const newTodo = createNewTodo(todos, query, USER_ID);
 
     setTodos(prevTodos => [...(prevTodos || []), newTodo]);
     setQuery('');
